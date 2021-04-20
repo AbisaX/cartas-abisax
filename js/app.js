b3477df3cb75f749f453c30d1d2f930e7fcc141b
AbisaX <73003791+AbisaX@users.noreply.github.com>
2021-04-19 21:47:09 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,52 @@
+
+const appOne = async (url, message = null) => {
+    if (url != null) {
+        try {
+            const ax = await axios(url);
+            console.log(ax.data[0].numero);
+        } catch (error) {
+            if (message != null) {
+                console.log(message);
+            } else {
+                console.log(`Error Not File Url: ${url}`);
+            }
+        }
+    }
+}
+
+const login = async (url, id, ic) => {
+    try {
+        let user = document.getElementById(id).value;
+        let clave = document.getElementById(ic).value;
+        let yoo = await axios(url);
+        if (Array.isArray(yoo.data)) {
+            yoo.data.forEach((e, i) => {
+                if (e.usuario === user) {
+                    if (e.clave === clave) {
+                        localStorage.setItem("usuario", e);
+                        window.location = "contenido.html";
+                    }else{
+                        alert("eres bruto o te haces");
+                    }
+                }else{
+                    alert("vete puto hacker");
+                }
+            });
+        }
+    }catch(error){
+console.log("se totio");
+    }
+}
+
+const butonLogin = (url, id, ic, ib) => {
+    let comoquiere = document.getElementById(ib);
+    comoquiere.addEventListener("click", () => {
+        login(url,id,ic);
+    });
+}
+
+const yaLogin = ()=>{
+if(localStorage.getItem("usuario")==null){
+    window.location="index.html";
+}
+}
